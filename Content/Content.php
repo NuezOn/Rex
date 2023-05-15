7c25ce7ccacd67b4d4ca5db89317b16338dfd40c
NuezOn <hectornuez1991@gmail.com>
2023-05-15 19:21:18 +0200

    first commit

---
+++ Content/Content.php
@@ -0,0 +1,6 @@
+<div class="content_section floating_element">
+    <?php include_once("Intro/Intro.html"); ?>          
+    <?php include_once("Configuration Overview/Configuration Overview.html"); ?>
+    <?php include_once("Document Roots/Document Roots.html"); ?>
+    <?php include_once("Reporting Problems/Reporting problems.html"); ?>
+</div>
